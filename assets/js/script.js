21456a3d401cd81ac8f580df553b6712ba42f36d
Adebayo Sotannde <148028245+tsotannde@users.noreply.github.com>
2025-11-19 22:04:35 -0600

    Structure
    Editing Structure of Navbar. Fixing the Order and Naviagtion to work as expected

--- assets/js/script.js
+++ assets/js/script.js
@@ -144,14 +144,14 @@ const pages = document.querySelectorAll("[data-page]");
 for (let i = 0; i < navigationLinks.length; i++) {
   navigationLinks[i].addEventListener("click", function () {
 
-    for (let i = 0; i < pages.length; i++) {
-      if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
-        pages[i].classList.add("active");
-        navigationLinks[i].classList.add("active");
+    for (let j = 0; j < pages.length; j++) {
+      if (i === j) {
+        pages[j].classList.add("active");
+        navigationLinks[j].classList.add("active");
         window.scrollTo(0, 0);
       } else {
-        pages[i].classList.remove("active");
-        navigationLinks[i].classList.remove("active");
+        pages[j].classList.remove("active");
+        navigationLinks[j].classList.remove("active");
       }
     }
 
